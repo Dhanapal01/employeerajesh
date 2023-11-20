8228f3435c5168e5cbd059407f16cd41ad46cd06
Dhanapal G <125639353+Dhanapal01@users.noreply.github.com>
2023-11-21 00:03:39 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot/service/EmployeeServiceImpl.java
@@ -0,0 +1,38 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        return optional.orElseThrow(() -> new RuntimeException("Employee not found for id :: " + id));
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        employeeRepository.deleteById(id);
+    }
+}
